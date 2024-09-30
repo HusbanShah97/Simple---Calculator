689626100cfe6053b4ea6d4f627b273526e35f80
HusbanShah97 <husbanshah97@gmail.com>
2024-09-30 17:37:56 +0500

    second commit

--- src/components/ui/calculator.tsx
+++ src/components/ui/calculator.tsx
@@ -1,2 +1,27 @@
 "use client";
-import
+import { useState, changeEvent } from "react";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input} from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+
+
+export default function Calculator() {
+    
+    const [Num1, setNum1] = useState<string>("");
+    const [Num2, setNum2] = useState<string>("");
+    const [Result, setResult] = useState<string>("");
+
+    const handleNum1Change = (e: changeEvent<HTMLInputElement>): void => {
+        setNum1(e.target.value);
+        
+    }
+
+    const handleNum2Change = (e: changeEvent<HTMLInputElement>): void => {
+        setNum2(e.target.value);
+        
+    }
+
+
+}
